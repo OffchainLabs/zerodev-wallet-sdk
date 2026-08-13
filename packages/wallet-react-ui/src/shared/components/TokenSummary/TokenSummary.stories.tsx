d0d10eff1a31b7826b65c7fd59f94a5176889304
Omar Basem <omarbasem.0101@gmail.com>
2026-08-13 13:25:04 +0400

    feat: TokenSummary component

---
+++ packages/wallet-react-ui/src/shared/components/TokenSummary/TokenSummary.stories.tsx
@@ -0,0 +1,33 @@
+import type { Meta, StoryObj } from '@storybook/react-vite'
+
+import { TokenSummary } from './index'
+
+const ETH_ICON =
+  'https://raw.githubusercontent.com/trustwallet/assets/master/blockchains/ethereum/info/logo.png'
+
+const meta = {
+  title: 'Shared/TokenSummary',
+  component: TokenSummary,
+  parameters: {
+    layout: 'centered',
+  },
+  tags: ['autodocs'],
+  decorators: [
+    (Story) => (
+      <div style={{ width: 352 }}>
+        <Story />
+      </div>
+    ),
+  ],
+} satisfies Meta<typeof TokenSummary>
+
+export default meta
+type Story = StoryObj<typeof meta>
+
+export const Default: Story = {
+  args: {
+    fiatValue: '+$170.27',
+    cryptoAmount: '0.0652 ETH',
+    tokenLogoUrl: ETH_ICON,
+  },
+}
